0906d9786087eeb0008df2e4414725eea07a55aa
ChinaZDC <chinazdc@live.cn>
2020-03-26 20:31:32 +0800

    初始化

---
+++ src/main/java/zdc/enterprise/SpringBootEnterpriseApplication.java
@@ -0,0 +1,13 @@
+package zdc.enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEnterpriseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEnterpriseApplication.class, args);
+    }
+
+}
