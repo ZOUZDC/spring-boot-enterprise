8bdb9b1117de098486dbd172c9902b2a61fad0ab
zoudechao <qwer1234>
2020-04-08 16:16:46 +0800

    参数验证 时间类型的转换

--- README.md
+++ README.md
@@ -3,8 +3,16 @@
 |序号|功能|需求|完成度|实现的功能|
 |---|---|---|---|---|
 |1|入参验证|入参验证及其异常捕获|30%||
+|2|入参时间类型的处理|date,localDate等类型的处理|100%|完成|
+
+
 
 
 `异常处理请看902全局异常处理`
 在这里`AllExceptionHandler`继承了`ResponseEntityExceptionHandler`重写了
 
+
+
+## 入参时间类型的处理
+实现了 LocalDate,LocalDateTime,LocalTime 正确格式的入参数据的转换
+实现了 Date类型 `yyyy-MM-dd` ,`yyyy-MM-dd HH:mm:ss` ,`second` ,`millsecond` 类型 正确格式的入参的数据转换  
--- src/test/java/zdc/enterprise/SpringBootEnterpriseApplicationTests.java
+++ src/test/java/zdc/enterprise/SpringBootEnterpriseApplicationTests.java
@@ -1,13 +1,37 @@
 package zdc.enterprise;
 
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+//@SpringBootTest
 class SpringBootEnterpriseApplicationTests {
 
     @Test
     void contextLoads() {
+
+
+        LocalDateTime now = LocalDateTime.now();
+        //获取秒数
+        Long second = now.toEpochSecond(ZoneOffset.of("+8"));
+//获取毫秒数
+        Long milliSecond = now.toInstant(ZoneOffset.of("+8")).toEpochMilli();
+
+
+        System.out.println(new Date(second));
+        System.out.println(new Date(milliSecond));
+
+
     }
 
 }
