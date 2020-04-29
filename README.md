50a64ff8d7e5b9bb30e6a378702390f609a4cb07
zoudechao <qwer1234>
2020-04-29 11:31:20 +0800

    集成redis

--- README.md
+++ README.md
@@ -2,4 +2,32 @@
 
 |序号|功能|需求|完成度|关联功能|
 |---|---|---|---|---|
-|1|基础接口|创建于2020年4月26的master|0%||
+|1|集成redis|集成redis|80%||
+
+### redis配置
+#### pom.xml
+``` 
+<dependency>
+   <groupId>org.springframework.boot</groupId>
+   <artifactId>spring-boot-starter-data-redis</artifactId>
+</dependency>
+```
+#### application.yml
+```
+spring: 
+  redis:
+    database: 0
+    host: 192.168.253.6
+    port: 6379
+    password: testdb
+```
+#### 指定序列化
+参看`MyRedisConfig`
+
+
+#### 简单测试
+
+测试结果在大量数据需要处理的时候建议使用`Pipeline`
+
+#### 建议
+建议将redis的常用命令封装为一个脚本,来解耦 ,如果是小型项目 就直接用吧
--- src/main/java/zdc/enterprise/constants/MyHandlerInterceptor.java
+++ src/main/java/zdc/enterprise/constants/MyHandlerInterceptor.java
@@ -1,6 +1,9 @@
 package zdc.enterprise.constants;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -13,12 +16,16 @@ import javax.servlet.http.HttpServletResponse;
 
 @Component
 @Order(-999)
+@Slf4j
 public class MyHandlerInterceptor implements HandlerInterceptor {
 
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Override
     public boolean preHandle(HttpServletRequest rq, HttpServletResponse response, Object handler ) throws Exception {
-
+        log.info("redis hasKey {}",redisTemplate.hasKey("123"));
         return true;
     }
 
