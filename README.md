097f097245c94edb09fc7de14d2dd2082f799a7a
zoudechao <qwer1234>
2020-07-01 16:54:50 +0800

    添加日志

--- README.md
+++ README.md
@@ -7,4 +7,13 @@
 
 
 ##使用说明
+不区分多环境的情况下使用logback.xml或者logback-spring.xml 均可
+当需要根据多环境的profile来分别配置logback的参数的话需要使用logback-spring.xml
 
+#### 
+根据application.yml中的位置,来指定生成日志文件位置
+```
+logging:
+  file:
+    path: ./logs/
+```
--- src/main/java/zdc/enterprise/SpringBootEnterpriseApplication.java
+++ src/main/java/zdc/enterprise/SpringBootEnterpriseApplication.java
@@ -1,13 +1,19 @@
 package zdc.enterprise;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringBootEnterpriseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootEnterpriseApplication.class, args);
+
+        for (int i = 0; i < 1000; i++) {
+            log.info("HELLO");
+        }
     }
 
 }
