2e8d242c6d46ad6b5051d5fa9c31d9fdf47cc947
ChinaZDC@live.cn <Qq654123105>
2021-01-11 21:05:29 +0800

    log4j的日志可以通过application来配置. 修复bug

--- src/main/java/zdc/enterprise/SpringBootEnterpriseApplication.java
+++ src/main/java/zdc/enterprise/SpringBootEnterpriseApplication.java
@@ -1,13 +1,22 @@
 package zdc.enterprise;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootEnterpriseApplication {
+@Slf4j
+public class SpringBootEnterpriseApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootEnterpriseApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        for (int i = 0; i < 100; i++) {
+            log.info("nihao");
+        }
+    }
 }
